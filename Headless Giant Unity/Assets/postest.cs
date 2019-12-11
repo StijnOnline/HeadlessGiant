60c8eefe8859408370921b99d117f9e362574b8b
StijnOnline <stijnvandeijzen@hotmail.nl>
2019-12-11 12:05:42 +0100

    Initial commit + first prototype

---
+++ Headless Giant Unity/Assets/postest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class postest : MonoBehaviour
+{
+    public float scale = 5;
+    public Transform controller;
+    public Transform VRrig;
+    
+    void Update()
+    {
+        transform.position = (controller.localPosition) * scale;
+        transform.rotation = controller.localRotation;
+    }
+}
---
+++ Headless Giant Unity/Assets/shake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class shake : MonoBehaviour
+{
+    public SteamVR_Action_Vibration vibrate_action;
+    public float delay;
+    public float duration;
+    public float freq;
+    public float amp;
+
+    [ContextMenu("SHAKE")]
+
+    public void Update() {
+        if(Input.GetMouseButton(0)) {
+            vibrate_action.Execute(delay, duration, freq, amp, SteamVR_Input_Sources.RightHand);
+
+        }
+    }
+    
+}

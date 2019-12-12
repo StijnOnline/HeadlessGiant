94d4511fbd5fc092a504eba4a40caaf3390163f4
StijnOnline <stijnvandeijzen@hotmail.nl>
2019-12-12 14:20:35 +0100

    Changed enemy behaviour

--- Headless Giant Unity/Assets/Enemy.cs
+++ Headless Giant Unity/Assets/Enemy.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshObstacle))]
 public class Enemy : MonoBehaviour {
     public float speed;
-    public NavMeshAgent navMeshAgent {get; private set;}
-    public Vector3 target = Vector3.zero;
+    public NavMeshAgent navMeshAgent { get; private set; }
+    public NavMeshObstacle navMeshObstacle { get; private set; }
+    private Vector3 target = Vector3.zero;
     public float targetDist;
 
     private void Start() {
+        navMeshObstacle = GetComponent<NavMeshObstacle>();
+        navMeshObstacle.enabled = false;
 
         navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.SetDestination(target);
         navMeshAgent.speed = speed;
+
+        
     }
 
     public void Die() {
@@ -22,11 +27,21 @@ public class Enemy : MonoBehaviour {
     }
 
     public void Update() {
-        float dist = Mathf.Sqrt(Mathf.Pow((transform.position-target).x, 2) + Mathf.Pow((transform.position-target).z, 2));
+        //float dist = Mathf.Sqrt(Mathf.Pow((transform.position - target).x, 2) + Mathf.Pow((transform.position - target).z, 2));
 
-        if(dist < targetDist) {
-            navMeshAgent.enabled = false;
+
+        if(navMeshAgent.isOnNavMesh) {
+
+            NavMeshHit hit;
+            if(NavMesh.SamplePosition(target - (target - transform.position).normalized * 0.1f, out hit, 5f, NavMesh.AllAreas)) {
+                navMeshAgent.SetDestination(hit.position);
+            }
+            if(navMeshAgent.remainingDistance != 0 && navMeshAgent.remainingDistance < targetDist) {
+                navMeshAgent.enabled = false;
+                navMeshObstacle.enabled = true;
+            }
         }
+
     }
 
 

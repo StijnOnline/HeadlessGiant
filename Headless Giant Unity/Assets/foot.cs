4b560e2aa9a912d5cf2bc04e33d35f67ccc31146
StijnOnline <stijnvandeijzen@hotmail.nl>
2019-12-13 12:36:26 +0100

    foot on leg

---
+++ Headless Giant Unity/Assets/foot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foot : MonoBehaviour
+{
+    public float scale = 5;
+    public Transform controller;
+    public Transform VRrig;
+    public Vector3 rotationOffset;
+    
+    void Update()
+    {
+        transform.position = (controller.localPosition) * scale;
+        Vector3 trackerRotation = controller.localRotation.eulerAngles + rotationOffset;
+        trackerRotation.z = 0;
+        trackerRotation.x *= 0.5f;
+        transform.rotation = Quaternion.Euler(trackerRotation);
+    }
+}

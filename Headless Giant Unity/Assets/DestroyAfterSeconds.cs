4d24f2679a57b957bcd076b31d59532c1300df75
Snorro1998 <tjaard.van.verseveld@gmail.com>
2019-12-15 15:40:11 +0100

    Enemies added and changed the behaviour of the spawner

---
+++ Headless Giant Unity/Assets/DestroyAfterSeconds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    public float deathTime = 4f;
+
+    private void Start()
+    {
+        Destroy(gameObject, deathTime);
+    }
+}

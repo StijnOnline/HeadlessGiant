53f56accc25372616544dde655eb300ba12d8be7
Snorro1998 <tjaard.van.verseveld@gmail.com>
2019-12-18 13:09:30 +0100

    Fixed Mage animation

--- Headless Giant Unity/Assets/Catapult.cs
+++ Headless Giant Unity/Assets/Catapult.cs
@@ -7,12 +7,12 @@ public class Catapult : MonoBehaviour
     public Transform arm;
     public Transform piv;
 
-    public float xRot, amp;
+    private float xRot, amp;
 
     public bool fire = false;
 
-    float timer = 0f;
-    float timerEnd = 1f;
+    private float timer = 0f;
+    private float timerEnd = 1f;
 
     Vector2 xRotRange = new Vector2(-25f, 90);
 
@@ -38,7 +38,7 @@ public class Catapult : MonoBehaviour
         xRot = amp * Mathf.Sin((2 * Mathf.PI) / 1 * (timer - 0.25f)) + (xRotRange.y - amp);
 
         arm.localRotation = Quaternion.Euler(new Vector3(xRot, 0, 0));
-        piv.localPosition = new Vector3(0, 0, -MapValue(-25, 90, 0, 1, xRot));
+        piv.localPosition = new Vector3(0, 0, -MapValue(xRotRange.x, xRotRange.y, 0, 1, xRot));
     }
 
     float MapValue(float fromLow, float fromHigh, float toLow, float toHigh, float value)

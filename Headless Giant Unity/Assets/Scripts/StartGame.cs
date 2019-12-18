c88060123e6ad9604df78096681b3a089585d48c
StijnOnline <stijnvandeijzen@hotmail.nl>
2019-12-18 21:01:12 +0100

    StartGame script and Info added

---
+++ Headless Giant Unity/Assets/Scripts/StartGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class StartGame : MonoBehaviour
+{
+    public List<Behaviour> toEnable;
+    public List<Behaviour> toDisable;
+    private bool started = false;
+    
+
+    public void Play() {        
+
+        if(started)
+            return;
+
+        foreach(Behaviour item in toEnable) {
+            item.enabled = true;
+        }
+
+        foreach(Behaviour item in toDisable) {
+            item.enabled = false;
+        }
+
+        started = true;
+    }
+
+}
+[CustomEditor(typeof(StartGame))]
+public class LookAtPointEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        StartGame myTarget = (StartGame)target;
+
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Play")) {
+            myTarget.Play();
+        }
+    }
+}
